90bd88133b6389b166e9d2814fa95f6464f697a6
Ervin Baccay <ebaccay@berkeley.edu>
2018-06-04 11:24:38 -0700

    Fixed the webapp! Now contains actual beige books data

--- BeigeDB/app.py
+++ BeigeDB/app.py
@@ -6,6 +6,9 @@ from flask import request, send_file
 import sqlite3
 import csv
 
+import datascience
+import numpy
+
 # Create the application.
 app = flask.Flask(__name__)
 app.static_folder = 'static'
@@ -41,40 +44,39 @@ def code():
     return flask.render_template('code.html')
 
 
+#Things to show: Date, Distctict, Sector Heading, Sector Text, First Keyword
 @app.route('/query.html')
 def query():
     format_ = request.args.get("format", None)
-    speaker = request.args.get("name", "")
+    keyword = request.args.get("name", "")
     year = request.args.get("year", "")
 
-    connection = sqlite3.connect("mydatabase.sqlite")
+    connection = sqlite3.connect("beigedb.sqlite")
     connection.row_factory = dictionary_factory
     cursor = connection.cursor()
 
     # Query that gets the records that match the query
-    all_records_query = "SELECT hearing.hearing_title as title, hearing.date as date, speech.text as text, \
-				speaker.surname as name FROM hearing inner join speech on \
-				speech.hearing_id = hearing.hearing_id inner join speaker \
-				on speaker.speech_id = speech.speech_id %s %s;"
+    all_records_query = "SELECT Date as date, District as district,\
+                        Heading as heading, Text as text,\
+                        First as first FROM BeigeDB %s %s;"
 
     where_clause = ""
-    if speaker or year:
-        where_clause = "where "
-        if speaker:
-            where_clause += " speaker.surname = ? " if speaker else ""
-        if year and speaker:
+    if keyword or year:
+        where_clause = "WHERE "
+        if keyword:
+            where_clause += " first = ? " if keyword else ""
+        if year and keyword:
             where_clause += " and "
         if year:
-            where_clause += " hearing.date like ? " if len(year) > 2 else ""
-    limit_statement = "limit 6" if format_ != "csv" else ""
+            where_clause += " date like ? " if len(year) > 2 else ""
+    limit_statement = "limit 8" if format_ != "csv" else ""
 
     all_records_query = all_records_query % (where_clause, limit_statement)
-    print(all_records_query)
 
-    if speaker and year:
-        cursor.execute(all_records_query, (speaker.lower(), "%" + year))
-    elif speaker:
-        cursor.execute(all_records_query, (speaker.lower(),))
+    if keyword and year:
+        cursor.execute(all_records_query, (keyword.lower(), "%" + year))
+    elif keyword:
+        cursor.execute(all_records_query, (keyword.lower(),))
     elif year:
         cursor.execute(all_records_query, ("%" + year,))
     else:
@@ -82,16 +84,14 @@ def query():
     records = cursor.fetchall()
 
     # Query to count the number of records
-    count_query = "SELECT count(*) as count FROM hearing inner join speech on \
-				speech.hearing_id = hearing.hearing_id inner join speaker \
-				on speaker.speech_id = speech.speech_id %s;"
-    count_query = count_query % (where_clause)
-    if speaker and year:
-        cursor.execute(count_query, (speaker.lower(), "%" + year))
+    count_query = "SELECT count(*) as count FROM BeigeDB %s;"
+    count_query = count_query % where_clause
+    if keyword and year:
+        cursor.execute(count_query, (keyword.lower(), "%" + year))
     elif year:
         cursor.execute(count_query, ("%" + year,))
-    elif speaker:
-        cursor.execute(count_query, (speaker.lower()))
+    elif keyword:
+        cursor.execute(count_query, (keyword.lower(),))
     else:
         cursor.execute(count_query)
     # There's a lot of if else going on here but I will send a better solution for you guys to work with
@@ -101,12 +101,12 @@ def query():
     # Send the information back to the view
     # if the user specified csv send the data as a file for download else visualize the data on the web page
     if format_ == "csv":
-        return download_csv(records, "speeches_%s.csv" % (speaker.lower()))
+        return download_csv(keyword, year)
     else:
-        years = [x for x in range(2018, 1995, -1)]
+        years = [x for x in range(2018, 1969, -1)]
         selected_year = int(year) if year else None
         return flask.render_template('query.html', records=records, no_of_records=no_of_records[0]['count'],
-                                     speaker=speaker, years=years, selected_year=selected_year)
+                                     keyword=keyword, years=years, selected_year=selected_year)
 
 
 ########################################################################
@@ -122,19 +122,33 @@ def dictionary_factory(cursor, row):
     return d
 
 
-def download_csv(data, filename):
+def download_csv(keyword, year):
     """
     Pass into this function, the data dictionary and the name of the file and it will create the csv file and send it to the view
     """
-    header = data[0].keys()  # Data must have at least one record.
-    with open('downloads/' + filename, "w+") as f:
-        writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(header)
-        for row in data:
-            writer.writerow(list(row.values()))
+    filename = "beige_db.csv"
+
+    beige_table = datascience.Table().read_table("beigedb.csv")
+
+    if year:
+        beige_table = beige_table.where("Date", lambda x: year in x)
+    if keyword:
+        beige_table = beige_table.where("First", keyword.lower())
+
+    header = beige_table.labels
+
+    f = open(filename, "w+", encoding='utf8')
+
+    writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(header)
+
+    for i in range(beige_table.num_rows):
+        new_row = list(beige_table.row(i))
+
+        writer.writerow(new_row)
 
     # Push the file to the view
-    return send_file('downloads/' + filename,
+    return send_file(filename,
                      mimetype='text/csv',
                      attachment_filename=filename,
                      as_attachment=True)
